66f7eb49d8d59c924ef25ea35c11070a2051f85e
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-06-07 14:10:35 -0400

    Example of nested loop

--- first.py
+++ first.py
@@ -33,19 +33,25 @@
 
 # Indentation example
 
-x = 5 
-if x > 2:
-    print("Bigger than 2")
-    print("Still bigger")
-print("Done with 2")
+# x = 5 
+# if x > 2:
+#     print("Bigger than 2")
+#     print("Still bigger")
+# print("Done with 2")
 
-for i in range(6):
-     print(i)
-     if i > 2:
-         print("Bigger than 2")
-print("Done with i", i)
-print("All done")
+# for i in range(6):
+#      print(i)
+#      if i > 2:
+#          print("Bigger than 2")
+# print("Done with i", i)
+# print("All done")
          
 
-        
-    
+# Nested loop
+
+x = 42 
+if x > 1:
+    print("More than 1")
+    if x < 100:
+        print("Less than 100")
+print("All done")        
