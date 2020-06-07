349eb8ee149d0dc1d65582f468bb0f0222d3198b
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-06-07 14:05:05 -0400

    Example of indentation with loop

--- first.py
+++ first.py
@@ -23,12 +23,29 @@
 # usFloorNumber = int(euFloorNumber) + 1
 # print("US floor uquivalent:", usFloorNumber) 
 
-x = 25
-if x < 10:
-    print("Smaller")
-if x  > 20:
-    print("Bigger")
+# x = 25
+# if x < 10:
+#     print("Smaller")
+# if x  > 20:
+#     print("Bigger")
     
-print("EndOfCode")
+# print("EndOfCode")
+
+# Indentation example
+
+x = 5 
+if x > 2:
+    print("Bigger than 2")
+    print("Still bigger")
+print("Done with 2")
+
+for i in range(6):
+     print(i)
+     if i > 2:
+         print("Bigger than 2")
+print("Done with i", i)
+print("All done")
+         
+
         
     
