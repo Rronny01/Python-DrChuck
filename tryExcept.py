e30702e5b2366e31646ba997e86a84db26a66890
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-06-07 17:55:57 -0400

    Sample try/except with user input

--- tryExcept.py
+++ tryExcept.py
@@ -1,21 +1,32 @@
 # Try-except error handling 1
 
-astr = "Hello bob"
-try:
-  istr = int(astr) # int() cant convert letters to int
-except:
-    istr = -1
+# astr = "Hello bob"
+# try:
+#   istr = int(astr) # int() cant convert letters to int
+# except:
+#     istr = -1
     
-print("First =", istr)   
+# print("First =", istr)   
 
-astr = "123"
-try:
-    istr = int(astr)
-except:
-    istr = int(astr)
-print("Second =", istr)     
+# astr = "123"
+# try:
+#     istr = int(astr)
+# except:
+#     istr = int(astr)
+# print("Second =", istr)     
        
+# Sample try/except with user input
 
+rawString = input("Enter a number: ")
+try:
+    inputValue = int(rawString)
+except:
+    inputValue = -1
+    
+if inputValue > 0:
+    print("Nice work")
+else:
+    print("Not a number")
  
 
 
