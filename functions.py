da1462a871062f0d04679eb796c7b6237a3355b8
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-06-07 18:55:14 -0400

    Parameters; with a placeholder/arguments

--- functions.py
+++ functions.py
@@ -8,13 +8,34 @@
 
 # Building our own function
 
-favoriteNumber = input("What is your favorite number: ")
-print("Your favorite number is:", favoriteNumber) 
+# favoriteNumber = input("What is your favorite number: ")
+# print("Your favorite number is:", favoriteNumber) 
 
-def myFavoriteNumber(): 
-    n = 5
-    while n > 0:
-        n = n-1
-print("My favorite number is:", myFavoriteNumber()) # n == 0; none
+# def myFavoriteNumber(): 
+#     n = 5
+#     while n > 0:
+#         n = n-1
+# print("My favorite number is:", myFavoriteNumber()) # n == 0; none
         
         
+# Parameters; with a placeholder/arguments 
+
+def greet(lang):
+    if lang == "es":
+        print("Hola")
+        
+    elif lang == "fr":
+        print("Bonjour")       
+        
+    else:
+        print("Hello")
+        
+greet("en")  
+greet("fr")
+greet("es")      
+        
+        
+        
+    
+
+
