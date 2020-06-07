d6f38839fd21c054a967d37a322c03eef7bf3fb6
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-06-07 13:32:27 -0400

    First file for course; initial commit

---
+++ first.py
@@ -0,0 +1,34 @@
+# print("Hello from Dr. Chuck")
+
+# x = 1 + 2 ** 3 / 4 * 5
+# print(x)
+#parenthesis> power> multi & div> add & subtract > left to right
+
+# n = 5
+# while n > 0:
+#     print(n)
+#     n = n-1
+# print("Game ends! Boom!!")
+
+
+# myString = "123"
+# strInt = int(myString) + 1
+# print(strInt)
+
+# name = input("What is your name? \nYour reply => ")
+# print("Guard => Welcome,", name)
+
+# euFloorNumber = input("EU floor: ")
+# #print(type(euFloorNumber))
+# usFloorNumber = int(euFloorNumber) + 1
+# print("US floor uquivalent:", usFloorNumber) 
+
+x = 25
+if x < 10:
+    print("Smaller")
+if x  > 20:
+    print("Bigger")
+    
+print("EndOfCode")
+        
+    
