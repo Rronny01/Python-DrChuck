ea84aad927db9873c9c5319e5a67fcb7b98a827e
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-06-16 23:46:51 -0400

    while loop; Infinite loop

---
+++ loopsIteraton-c5.py
@@ -0,0 +1,22 @@
+# while loop
+
+n = 5
+while n > 0:
+    print(n)
+    n = n-1
+print("loop ends!")
+print(n)
+
+# Infinite loop
+
+n = 5 
+
+while n > 0:
+    print("Soap")
+    print("Rinse")
+print("Dry off")
+    
+    
+    
+    
+    
