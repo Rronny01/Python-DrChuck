b8509b0695c9cf3cf6f3d86cfe555c14b4a9046c
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-06-16 23:56:59 -0400

    # Breaking out of a loop

--- loopsIteraton-c5.py
+++ loopsIteraton-c5.py
@@ -1,21 +1,30 @@
 # while loop
 
-n = 5
-while n > 0:
-    print(n)
-    n = n-1
-print("loop ends!")
-print(n)
+# n = 5
+# while n > 0:
+#     print(n)
+#     n = n-1
+# print("loop ends!")
+# print(n)
 
 # Infinite loop
 
-n = 5 
+# n = 5 
 
-while n > 0:
-    print("Soap")
-    print("Rinse")
-print("Dry off")
-    
+# while n > 0:
+#     print("Soap")
+#     print("Rinse")
+# print("Dry off")
+
+
+# Breaking out of a loop
+
+while True:
+    line = input("> ")
+    if line == "done" :
+        break
+    print(line)
+print("Done!")    
     
     
     
